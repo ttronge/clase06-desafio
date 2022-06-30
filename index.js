965e1e59dc78481300e67c164a9ccf178d4378e5
ttronge <tomastronge@gmail.com>
2022-06-30 00:25:14 -0300

    add env

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT ||8080;
 
 // utils
 const productos = JSON.parse(fs.readFileSync('./productos.txt'))
